ad4f7ba6881477dcf37eb452f44f42cce9b53acc
zilto <tjean@DESKTOP-V6JDCS2.localdomain>
2023-05-08 17:16:50 -0400

    clean esbuild script

--- esbuild.js
+++ esbuild.js
@@ -16,84 +16,64 @@ const extensionConfig = {
     external: ["vscode"],
 };
 
-(async () => {
-    try {
-        await build(extensionConfig);
-        console.log("build complete");
-    } catch (err) {
-        process.stderr.write(err.stderr);
-        process.exit(1);
-    }
-})();
-
 const watchConfig = {
-    watch: {
-        onRebuild(error, result) {
-        console.log("[watch] build started");
-        if (error) {
-            error.errors.forEach(error =>
-            console.error(`> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`)
-            );
-        } else {
-            console.log("[watch] build finished");
-        }
-        },
+  watch: {
+    onRebuild(error, result) {
+    console.log("[watch] build started");
+    if (error) {
+      error.errors.forEach(error =>
+      console.error(`> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`)
+      );
+    } else {
+      console.log("[watch] build finished");
+    }
     },
+  },
 };
 
-(async () => {
-    const args = process.argv.slice(2);
-    try {
-        if (args.includes("--watch")) {
-        // Build and watch source code
-        console.log("[watch] build started");
-        await build({
-            ...extensionConfig,
-            ...watchConfig,
-        });
-        console.log("[watch] build finished");
-        } else {
-        // Build source code
-        await build(extensionConfig);
-        console.log("build complete");
-        }
-    } catch (err) {
-        process.stderr.write(err.stderr);
-        process.exit(1);
-    }
-})();
-
 const webviewConfig = {
-    ...baseConfig,
-    target: "es2020",
-    format: "esm",
-    entryPoints: ["./src/webview/main.ts"],
-    outfile: "./out/webview.js",
-  };
+  ...baseConfig,
+  target: "es2020",
+  format: "esm",
+  entryPoints: ["./src/webview/main.ts"],
+  outfile: "./out/webview.js",
+};
+
+const rendererConfig = {
+  ...baseConfig,
+  target: "es2020",
+  format: "esm",
+  entryPoints: ["./src/notebook/renderer.ts"],
+  outfile: "./out/renderer.js",
+};
   
-  (async () => {
-    const args = process.argv.slice(2);
-    try {
-      if (args.includes("--watch")) {
-        // Build and watch extension and webview code
-        console.log("[watch] build started");
-        await build({
-          ...extensionConfig,
-          ...watchConfig,
-        });
-        await build({
-          ...webviewConfig,
-          ...watchConfig,
-        });
-        console.log("[watch] build finished");
-      } else {
-        // Build extension and webview code
-        await build(extensionConfig);
-        await build(webviewConfig);
-        console.log("build complete");
-      }
-    } catch (err) {
-      process.stderr.write(err.stderr);
-      process.exit(1);
+(async () => {
+  const args = process.argv.slice(2);
+  try {
+    if (args.includes("--watch")) {
+      // Build and watch extension and webview code
+      console.log("[watch] build started");
+      await build({
+        ...extensionConfig,
+        ...watchConfig,
+      });
+      await build({
+        ...webviewConfig,
+        ...watchConfig,
+      });
+      await build({
+        ...rendererConfig,
+        ...watchConfig,
+      });
+      console.log("[watch] build finished");
+    } else {
+      // Build extension and webview code
+      await build(extensionConfig);
+      await build(webviewConfig);
+      console.log("build complete");
     }
-  })();
+  } catch (err) {
+    process.stderr.write(err.stderr);
+    process.exit(1);
+  }
+})();
